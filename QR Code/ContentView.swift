3e1ffc9f3a4e4ced3533df8294f07415e1dfc124
realzare <realhosseinzare@gmail.com>
2024-08-28 16:39:27 +0330

    Initial Commit

---
+++ QR Code/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  QR Code
+//
+//  Created by Hossein Zare on 8/28/24.
+//
+
+import SwiftUI
+import CoreImage.CIFilterBuiltins
+
+struct ContentView: View {
+    @State private var name = ""
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                LinearGradient(colors: [.indigo, .blue], startPoint: .topLeading, endPoint: .bottomTrailing)
+                    .ignoresSafeArea()
+        
+                VStack {
+                    TextField("Name", text: $name)
+                        .textFieldStyle(.roundedBorder)
+                        .textContentType(.name)
+                        .font(.title)
+                        .padding(10)
+                    
+            Image(uiImage:generateQRCode(_: "\(name)"))
+                        .interpolation(.none)
+                        .resizable()
+                        .scaledToFit()
+                        .padding(80)
+                }
+                    .navigationTitle("QR Code")
+                }
+            }
+        }
+    
+    func generateQRCode(_ string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+        
+        if let outputImage = filter.outputImage {
+        if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgImage)
+            }
+        }
+        
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
---
+++ QR Code/QR_CodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_CodeApp.swift
+//  QR Code
+//
+//  Created by Hossein Zare on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct QR_CodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
